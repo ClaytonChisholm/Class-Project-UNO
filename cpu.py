a9324377daa4c29154ab67e0c5d19e2aa75dd987
laurenpaicopolis <44977115+laurenpaicopolis@users.noreply.github.com>
2020-10-19 19:02:53 -0400

    More merge problems with main

--- cpu.py
+++ cpu.py
@@ -1,4 +1,6 @@
 import random
+from card import *
+
 
 class CPU:
     CPU_hand = []
@@ -24,30 +26,50 @@ class CPU:
         return CPU_hand
 
     def play_card(self, Player, CPU1, CPU2, CPU3):
+        global CPU_hand
         special_cards = []
+        power_card_found = False
         player_hand = Player.get_hand()
         player_length = len(player_hand)
         CPU1_hand = CPU1.get_hand()
         CPU2_hand = CPU2.get_hand()
         CPU3_hand = CPU3.get_hand()
+
         if player_length <= 3 or CPU1_hand <= 3 or CPU2_hand <= 3 or CPU3_hand <= 3:
             for card in CPU_hand:
-                if card.get_number() == "Draw 4":
+                if card.get_type() == Type.DRAW4:
+                    power_card_found = True
                     special_cards.append(card)
-                elif card.get_number() == 'Reverse':
+                elif card.get_type() == Type.REVERSE:
+                    power_card_found = True
                     special_cards.append(card)
-                elif card.get_number() == 'Draw 2':
+                elif card.get_type() == Type.DRAW2:
+                    power_card_found = True
                     special_cards.append(card)
-                elif card.get_number() == 'Skip':
+                elif card.get_type() == Type.SKIP:
+                    power_card_found = True
                     special_cards.append(card)
 
-            power_card_length = len(special_cards)
-            card_chosen = random.randint(0, power_card_length)
-            card_selected = special_cards[card_chosen]
-            return card_selected
-
+            if power_card_found:
+                power_card_length = len(special_cards)
+                card_chosen = random.randint(0, power_card_length)
+                card_selected = special_cards[card_chosen]
+                return card_selected
+            else:
+                num_left = len(CPU_hand)
+                card_num = random.randint(0, num_left)
+                played_card = CPU_hand.pop(card_num)
+                return played_card
         else:
-            #finishing this part tomorrow
+            num_left = len(CPU_hand)
+            card_num = random.randint(0, num_left)
+            played_card = CPU_hand.pop(card_num)
+            return played_card
+
+    # if card randomly chosen is not valid, the CPU_hand has to re-insert the card back
+    # into its deck
+    def reinsert_card(self, card):
+        CPU_hand.append(card)
 
 
 
